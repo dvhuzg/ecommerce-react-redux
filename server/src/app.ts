ec6b8a62fa2ac47d72483b6a9ba39922b4c34ba1
dvhuzg <dohungdvh@gmail.com>
2023-01-13 00:04:20 +0700

    test commit from home PC

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.send("Dohung");
+  return res.send("DoHung");
 });
 app.listen(3000, () => {
   console.log("Application is running on https://localhost:3000");

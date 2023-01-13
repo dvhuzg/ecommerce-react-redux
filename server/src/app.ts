1d8e6343ffde24814a1e40a4ec83f82168892fb0
DoHung <dohungdvh@gmail.com>
2023-01-13 09:37:44 +0700

    Express interfaces

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (rreq: Request, res: Response) => {
   return res.send("DoHung");
 });
-app.post("/api/data", (req, res) => {
+app.post("/api/data", (req: Request, res: Response) => {
   console.log(req.body);
   return res.sendStatus(200);
 });
-app.all("/api/all", (req, res) => {
+app.all("/api/all", (req: Request, res: Response) => {
   return res.sendStatus(200);
 });
 app.listen(3000, () => {
